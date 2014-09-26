73b5e0b6c3767d84bf6bd9adbf686bc27e2ee53e
wade-sam <40404@longroad.ac.uk>
2014-09-26 16:07:37 +0100

    Class exercise 1

---
+++ Class exercise 1.py
@@ -0,0 +1,29 @@
+#john bain
+#selection improvement exercise
+#26-06-12
+
+month = int(input("Please enter a month as a number between 1-12: "))
+if month == 1:
+    print("The month you entered is January")
+elif month == 2:
+    print("The month you entered is February")
+if month == 3:
+    print("The month you entered is March")
+if month == 4:
+    print("The month you entered is April")
+if month == 5:
+    print("The month you entered is May")
+if month == 6:
+    print("The month you entered is June")
+if month == 7:
+    print("The month you entered is July")
+if month == 8:
+    print("The month you entered is August")
+if month == 9:
+    print("The month you entered is September")
+if month == 10:
+    print("The month you entered is October")
+if month == 11:
+    print("The month you entered is November")
+if month == 12:
+    print("The month you entered is December")
