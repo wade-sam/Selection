6446f2f300cc56f816daca383c2db5fcb6231959
wade-sam <40404@longroad.ac.uk>
2014-09-30 11:16:00 +0100

    exercise 2

---
+++ developmet exercise 2.py
@@ -0,0 +1,14 @@
+#sam wade
+#developmet exercise exercise 2 30/9/14
+
+water_temp = int(input("please enter the temperature of the water in degrees cenigrade:"))
+if water_temp >= 100:
+    print("The water is boiling")
+elif water_temp <= 0:
+    print("the water is frozen")
+else:
+     print("the water is neiter frozen or boiling")
+                       
+
+                       
+    
